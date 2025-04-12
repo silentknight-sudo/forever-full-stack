6578032c2b1a13fbb3cd0ab84971e10ca83662cd
Vivek Shukla <71977466+silentknight-sudo@users.noreply.github.com>
2025-04-12 21:41:27 +0530

    gg

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {port : 5174}
+  server: {port:5174},
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: true
+  },
+  base: '/admin/'
 })
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {port:5173}
+  server: {port:5173},
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: true
+  },
+  base: '/'
 })
